1f0d1621298e327143495526d0707e233902f180
MrSurfinbird <simon.marcus.monie@gmail.com>
2018-05-14 15:57:51 +0200

    fixed timeout for server

--- server.c
+++ server.c
@@ -58,12 +58,14 @@ int main() {
 	int receiveStatus;
 	struct client client = { 0 };
 	struct packet packet;
-	struct sockaddr_in source;
-	
+	struct sockaddr_in otherAddress;
 	int resendCount = 0;
 	int slidingWindowSize = WINDOW_SIZE;
 	int slidingWindowIndexFirst = 0, slidingWindowIndexLast = 0;
 	struct packet *windowBuffer[BUFFER_SIZE];
+	TimerList timerList = NULL;
+	struct timespec start, stop;
+	time_t deltaTime = 0, tTimeout = 0;
 	
 	createPacket(&packet, SYN, 0, 100, 200, "bla");
 	if (!isPacketBroken(&packet))
@@ -81,24 +83,21 @@ int main() {
 			case LISTEN:
 				if (receiveStatus == RECEIVE_OK && packet.flags == SYN) {
 					createPacket(&packet, SYNACK, 0, 0, 0, NULL);
-					sendPacket(mySocket, &packet, &source, 0);
+					sendPacket(mySocket, &packet, &otherAddress, 0);
+					addPacketTimer(&timerList, &packet, &otherAddress, deltaTime, TIMEOUT);
 					printf(ANSI_WHITE"LISTEN GOING TO SYN_RECEIVED\n"ANSI_RESET);
 					state = SYN_RECEIVED;
 				}
 				break;
-			
 			case SYN_RECEIVED:
 				if (resendCount >= MAX_RESENDS){
+					removePacketTimerBySeq(&timerList, packet.seq);
 					resendCount = 0;
 					state = INIT;
 					printf(ANSI_WHITE"CONNECTION TIMEOUT GOING TO CLOSED\n"ANSI_RESET);
 				}
-				else if (receiveStatus == RECEIVE_TIMEOUT){//receiveStatus -> timeout
-					createPacket(&packet, SYNACK, 0, 0, 0, NULL);
-					sendPacket(mySocket, &packet, &source, 1);
-					resendCount++;
-				}
 				else if (packet.flags == ACK){
+					removePacketTimerBySeq(&timerList, packet.seq);
 					resendCount = 0;
 					state = WAIT;
 					printf(ANSI_WHITE"CONNECTION SUCCESS GOING TO DATA TRANSMISSION\n"ANSI_RESET);
@@ -122,7 +121,7 @@ int main() {
 									}
 									else{
 										createPacket(&packet, ACK, 0, 0, 0, NULL);
-										sendPacket(mySocket, &packet, &source, 0);
+										sendPacket(mySocket, &packet, &otherAddress, 0);
 										printf(ANSI_WHITE"FRAME_RECEIVED GOING TO WAIT - FRAME OUTSIDE WINDOW\n"ANSI_RESET);
 										state = WAIT;
 									}
@@ -134,9 +133,9 @@ int main() {
 									}
 									else{
 										createPacket(&packet, ACK, 0, 0, 0, NULL);
-										sendPacket(mySocket, &packet, &source, 0);
+										sendPacket(mySocket, &packet, &otherAddress, 0);
 										printf(ANSI_WHITE"FRAME_RECEIVED GOING TO WAIT - FRAME OUTSIDE WINDOW\n"ANSI_RESET);
-										printf(ANSI_GREEN"OUT D ACK sent to "ANSI_BLUE"%s"ANSI_GREEN":"ANSI_BLUE"%d\n"ANSI_RESET, inet_ntoa(source.sin_addr), ntohs(source.sin_port));
+										printf(ANSI_GREEN"OUT D ACK sent to "ANSI_BLUE"%s"ANSI_GREEN":"ANSI_BLUE"%d\n"ANSI_RESET, inet_ntoa(otherAddress.sin_addr), ntohs(otherAddress.sin_port));
 										state = WAIT;
 									}
 								}
@@ -145,24 +144,14 @@ int main() {
 							case FRAME_IN_WINDOW:
 								if(packet.seq == slidingWindowIndexFirst){
 									createPacket(&packet, ACK, 0, 0, 0, NULL);
-									sendPacket(mySocket, &packet, &source, 0);
+									sendPacket(mySocket, &packet, &otherAddress, 0);
 									printf(ANSI_WHITE"FRAME_IN_WINDOW GOING TO MOVE_WINDOW - FRAME IS FIRST\n"ANSI_RESET);
 									state = MOVE_WINDOW;
 								}
 								break;
 							case MOVE_WINDOW:
-								if(slidingWindowIndexFirst != BUFFER_SIZE-1 && slidingWindowIndexLast != BUFFER_SIZE-1){
-									slidingWindowIndexFirst++;
-									slidingWindowIndexLast++;
-								}
-								else if(slidingWindowIndexFirst == BUFFER_SIZE-1){
-									slidingWindowIndexFirst = 0;
-									slidingWindowIndexLast++;
-								}
-								else if(slidingWindowIndexLast == BUFFER_SIZE-1){
-									slidingWindowIndexLast = 0;
-									slidingWindowIndexFirst++;
-								}
+								slidingWindowIndexFirst = (slidingWindowIndexFirst+1) % BUFFER_SIZE;
+								slidingWindowIndexLast = (slidingWindowIndexLast+1) % BUFFER_SIZE;
 								printf(ANSI_WHITE"MOVE_WINDOW GOING TO BUFFER\n"ANSI_RESET);
 								state = BUFFER;
 							
@@ -185,7 +174,7 @@ int main() {
 				
 				else if (receiveStatus == RECEIVE_OK && packet.flags == FIN){
 					createPacket(&packet, ACK, 0, 0, 0, NULL);
-					sendPacket(mySocket, &packet, &source, 0);
+					sendPacket(mySocket, &packet, &otherAddress, 0);
 					printf(ANSI_WHITE"DATA RECEIVE STATE GOING TO CLOSE_WAIT\n"ANSI_RESET);
 					state = CLOSE_WAIT;
 					}
@@ -195,23 +184,21 @@ int main() {
 		
 			case CLOSE_WAIT:
 				createPacket(&packet, FIN, 0, 0, 0, NULL);
-				sendPacket(mySocket, &packet, &source, 0);
+				sendPacket(mySocket, &packet, &otherAddress, 0);
+				addPacketTimer(&timerList, &packet, &otherAddress, deltaTime, TIMEOUT);
 				printf(ANSI_WHITE"CLOSE_WAIT GOING TO LAST_ACK\n"ANSI_RESET);
 				state = LAST_ACK;
 				break;
 			
 			case LAST_ACK:
 				if (resendCount >= MAX_RESENDS){
+					removePacketTimerBySeq(&timerList, packet.seq);
 					resendCount = 0;
 					state = INIT;
 					printf(ANSI_WHITE "MAX RESENDS REACHED - LAST_ACK GOING TO CLOSED\n"ANSI_RESET);
 				}
-				else if (receiveStatus == RECEIVE_TIMEOUT){ // TIMEOUT
-					createPacket(&packet, FIN, 0, 0, 0, NULL);
-					sendPacket(mySocket, &packet, &source, 1);
-					resendCount++;
-				}
 				else if (packet.flags == ACK){
+					removePacketTimerBySeq(&timerList, packet.seq);
 					resendCount = 0;
 					state = INIT;
 					printf(ANSI_WHITE"CONNECTION SHUTDOWN SUCCESSFULLY - LAST_ACK GOING TO CLOSED\n"ANSI_RESET);
@@ -223,7 +210,7 @@ int main() {
 				break;
 		}
 		fflush(stdout);
-		receiveStatus = receivePacketOrTimeout(mySocket, &packet, &source, TIMEOUT);
+		receiveStatus = receivePacketOrTimeout(mySocket, &packet, &otherAddress, TIMEOUT);
 	}
 	return 0;
 }
